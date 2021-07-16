3e872200c6acde618ddc18877e9cf70c95590e00
Kartikey Garg <56454264+Kartikey-Garg@users.noreply.github.com>
2021-07-16 11:50:15 +0530

    Add files via upload

---
+++ WebDeploy.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pickle
+from flask import Flask, request, render_template
+model = pickle.load(open('C:/Users/Kartikey Garg/Documents/Resources/HIV Dangue All Diseses/Datasets/archive/model.pkl', 'rb')) 
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('Disease Prediction.html')
+
+# Bind predict function to URL
+@app.route('/predict', methods =['POST'])
+def predict():
+    features = [float(i) for i in request.form.values()]
+    array_features = [np.array(features)]
+    prediction = model.predict(array_features)
+    print(prediction)
+    output = prediction
+    
+    if output == 1:
+        return render_template('Disease Prediction.html', 
+                               result = 'The patient is not likely to have disease!')
+    else:
+        return render_template('Disease Prediction.html', 
+                               result = 'The patient is likely to have disease!')
+if __name__ == '__main__':
+    app.run()
+web: gunicorn app:app
